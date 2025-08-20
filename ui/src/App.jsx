945ffc8f7d22281fcdbdeccdad30cbaf203d383e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-20 18:56:30 +0100

    init ui

---
+++ ui/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+/*
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+*/
+function App() {
+
+  return (
+    <>
+     <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
